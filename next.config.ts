e00b8ed572874eabfbbd93422b215a7ec0e4be52
janaka99 <janakachamith88@gmail.com>
2025-09-30 18:38:16 +0530

    ll

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    optimizePackageImports: ["@prisma/client"],
+  },
   serverActions: {
     bodySizeLimit: "10mb", // increase to 10 MB (or any size you need)
   },
